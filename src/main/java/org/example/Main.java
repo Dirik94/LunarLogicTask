12bd100e77dbab9b8ec93040afccd8b16874ce89
Grzegorz Domanowski <grzegorz.domanowski.ais@gmail.com>
2022-10-15 17:49:23 +0200

    ver 2.0

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static void main(String[] args) {
 
         int input;
+        int change = 6;
 
         ArrayList<ArrayList<Integer>> listOfArrays = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
@@ -25,6 +26,37 @@ public class Main {
             listOfArrays.add(numbersArray);
             sc.next();
         }
-        System.out.println(Arrays.toString(listOfArrays.toArray()));
+        System.out.println("Your numbers are: " + Arrays.toString(listOfArrays.toArray()));
+
+        for (int i = 0; i < listOfArrays.size(); i++){
+            ArrayList<Integer> arr = listOfArrays.get(i);
+            for (int x = 0; x < arr.size(); x++){
+                if (arr.get(x) % 9 == 0){
+                    break;
+                }
+                if (arr.get(x) % 3 == 2) {
+                    int x2 = arr.get(x);
+                    x2 += 1;
+                    arr.set(arr.indexOf(arr.get(x)),x2);
+                    change -= 1;
+                }
+            }
+        }
+        for (int i = 0; i < listOfArrays.size(); i++) {
+            ArrayList<Integer> arr = listOfArrays.get(i);
+            for (int x = 0; x < arr.size(); x++) {
+                while (change > 0) {
+                    if (arr.get(x) % 3 == 1) {
+                        int x2 = arr.get(x);
+                        x2 += 2;
+                        arr.set(arr.indexOf(arr.get(x)), x2);
+                        change -= 2;
+                    }else {
+                        break;
+                    }
+                }
+            }
+        }
+        System.out.println("Your numbers after task are: " + Arrays.toString(listOfArrays.toArray()));
     }
 }
