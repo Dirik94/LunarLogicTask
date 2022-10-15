bfd9359545dd7c9b594b3211cada5c69076ca94d
Grzegorz Domanowski <grzegorz.domanowski.ais@gmail.com>
2022-10-15 18:27:09 +0200

    ver 2.1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,55 +8,47 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        int input;
-        int change = 6;
+        int input, change = 6;
 
-        ArrayList<ArrayList<Integer>> listOfArrays = new ArrayList<>();
+        ArrayList<Integer> numbersArray = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
-        System.out.println("Type number of Arrays");
-        int numberOfArrays = sc.nextInt();
-        for (int i = 0; i < numberOfArrays; i++) {
-            ArrayList<Integer> numbersArray = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
             System.out.println("To end type any letter...");
             System.out.println("Type numbers:");
-            while (sc.hasNextInt()) {
-                input = sc.nextInt();
-                numbersArray.add(input);
-            }
-            listOfArrays.add(numbersArray);
-            sc.next();
+            input = sc.nextInt();
+            numbersArray.add(input);
         }
-        System.out.println("Your numbers are: " + Arrays.toString(listOfArrays.toArray()));
+        System.out.println("Your numbers are: " + Arrays.toString(numbersArray.toArray()));
 
-        for (int i = 0; i < listOfArrays.size(); i++){
-            ArrayList<Integer> arr = listOfArrays.get(i);
-            for (int x = 0; x < arr.size(); x++){
-                if (arr.get(x) % 9 == 0){
+        for (int i = 0; i < numbersArray.size(); i++){
+            if (numbersArray.get(i) % 9 == 0){
+                break;
+            }
+            if (numbersArray.get(i) % 3 == 2) {
+                int x2 = numbersArray.get(i);
+                x2 += 1;
+                numbersArray.set(numbersArray.indexOf(numbersArray.get(i)),x2);
+                change -= 1;
+            }
+            while (change > 0) {
+                if (numbersArray.get(i) % 3 == 1) {
+                    int x2 = numbersArray.get(i);
+                    x2 += 2;
+                    numbersArray.set(numbersArray.indexOf(numbersArray.get(i)), x2);
+                    change -= 2;
+                }else {
                     break;
                 }
-                if (arr.get(x) % 3 == 2) {
-                    int x2 = arr.get(x);
-                    x2 += 1;
-                    arr.set(arr.indexOf(arr.get(x)),x2);
-                    change -= 1;
+            }
+            if (change != 0) {
+                if (numbersArray.get(i) % 9 != 0){
+                    int x2 = numbersArray.get(i);
+                    x2 += change;
+                    change = 0;
+                    numbersArray.set(numbersArray.indexOf(numbersArray.get(i)),x2);
                 }
             }
         }
-        for (int i = 0; i < listOfArrays.size(); i++) {
-            ArrayList<Integer> arr = listOfArrays.get(i);
-            for (int x = 0; x < arr.size(); x++) {
-                while (change > 0) {
-                    if (arr.get(x) % 3 == 1) {
-                        int x2 = arr.get(x);
-                        x2 += 2;
-                        arr.set(arr.indexOf(arr.get(x)), x2);
-                        change -= 2;
-                    }else {
-                        break;
-                    }
-                }
-            }
-        }
-        System.out.println("Your numbers after task are: " + Arrays.toString(listOfArrays.toArray()));
+        System.out.println("Your numbers after task are: " + Arrays.toString(numbersArray.toArray()));
     }
 }
